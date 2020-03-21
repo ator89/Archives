b589f4d84b5380c0e882ee064f33d57a0c9ca15e
antox89 <ator89@outlook.com>
2020-03-21 11:39:47 -0600

    TDA file

--- Archives/src/archives/TDA_Archivo.java
+++ Archives/src/archives/TDA_Archivo.java
@@ -1,14 +1,26 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package archives;
+
+import java.io.File;
 
 /**
  *
  * @author angel
  */
 public class TDA_Archivo {
+    
+    File archivo = null;
+
+    public TDA_Archivo(String path) {
+        archivo = new File(path);
+    }
+
+    public File getArchivo() {
+        return archivo;
+    }
+
+    public void setArchivo(File archivo) {
+        this.archivo = archivo;
+    }
     
 }
